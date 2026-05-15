f013ebd130d3f1c055c033bf3f8731bf567dd681
Joshua Luczon <jluczon7264@gmail.com>
2026-05-15 10:44:45 -0400

    changed .py files for autograder idk anymore

--- lab2c.py
+++ lab2c.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
-
-name = input('Name: ')
-age = input('Age: ')
+import sys
+name = sys.argv[1]
+age = sys.argv[2]
 print('Hi ' + name + ', you are ' + str(age) + ' years old.')
-
